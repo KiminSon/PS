7377c4613b30886635e3334fdcd94417e2dcbce3
KiminSon <123408986+KiminSon@users.noreply.github.com>
2023-12-25 18:40:18 +0900

    [Bronze V] Title: 연세대학교, Time: 112 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n == 0)
+            System.out.print("YONSEI");
+        else
+            System.out.println("Leading the Way to the Future");
+    }
+}
