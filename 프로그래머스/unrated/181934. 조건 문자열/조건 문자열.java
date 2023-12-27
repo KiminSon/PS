cf8bf41e80d81a6c1d56c2ebcb7bb4c483eb5c78
KiminSon <123408986+KiminSon@users.noreply.github.com>
2023-12-27 13:00:15 +0900

    [level unrated] Title: 조건 문자열, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {        
+        if(eq.equals("=") && n == m){
+            return 1; 
+        }
+        
+        if(ineq.equals("<") && n < m){
+            return 1;
+        }
+        
+        if(ineq.equals(">") && n > m){
+            return 1;
+        }
+        return 0;
+    }
+}
