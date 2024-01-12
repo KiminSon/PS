c76fd3ad61679d0b9e1015fcca9e00fde5ef5bd1
KiminSon <123408986+KiminSon@users.noreply.github.com>
2024-01-12 14:49:13 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    while (n--) {
+        for (int i = 0; i <= n; i++)
+            cout << '*';
+        cout << '\n';
+    }
+}
