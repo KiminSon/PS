792e84a816a9b1c5e147bb5b879a3c66f244da3e
KiminSon <123408986+KiminSon@users.noreply.github.com>
2024-01-20 20:24:39 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    char c;
+    cin >> c;
+    if (c == 'N' || c == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+}
