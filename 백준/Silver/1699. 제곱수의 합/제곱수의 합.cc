d593d8e3438421f5908fce386838c45e22006867
KiminSon <123408986+KiminSon@users.noreply.github.com>
2024-01-25 16:56:38 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int n, dp[100001];
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+        dp[i] = i;
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j * j <= i; j++)
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+    cout << dp[n];
+}
