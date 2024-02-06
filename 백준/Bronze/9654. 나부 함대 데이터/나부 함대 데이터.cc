cc983006f20d732cef274ed3f3c01991d2598496
KiminSon <123408986+KiminSon@users.noreply.github.com>
2024-02-06 17:23:28 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+            "N2 Bomber      Heavy Fighter  Limited    21        \n"
+            "J-Type 327     Light Combat   Unlimited  1         \n"
+            "NX Cruiser     Medium Fighter Limited    18        \n"
+            "N1 Starfighter Medium Fighter Unlimited  25        \n"
+            "Royal Cruiser  Light Combat   Limited    4         ";
+}
