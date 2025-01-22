6ce4a2d7fd53104372b35e0d96c478597d29e9db
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-23 00:48:53 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 34848 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -6,6 +6,7 @@ public class Main {
 
     private static int n, m;
     private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +16,15 @@ public class Main {
         int[] nums = new int[m];
         visited = new boolean[n + 1];
         f(0, 0, nums);
+        System.out.print(sb.toString());
     }
 
     private static void f(int depth, int cnt, int[] nums) {
         if (depth == m) {
-            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < nums.length; i++) {
                 sb.append(nums[i] + " ");
             }
-            System.out.println(sb.toString());
+            sb.append("\n");
             return;
         }
         for (int i = 1; i <= n; i++) {
