6fd2d91869860ae6616b747124aeab0fc651ba5c
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-26 00:08:46 +0900

    [Silver III] Title: N과 M (4), Time: 176 ms, Memory: 19596 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -4,9 +4,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] p;
     static int n, m;
-    static int[] nums;
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
@@ -14,32 +13,22 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        p = new int[n];
-        for (int i = 0; i < n; i++) {
-            p[i] = i + 1;
-        }
-        nums = new int[m];
-        perm(0);
+        arr = new int[m];
+        f(0, 1);
         System.out.println(sb.toString());
     }
 
-    private static void perm(int depth) {
+    private static void f(int depth, int num) {
         if (depth == m) {
-            for (int i = 1; i < m; i++) {
-                if (nums[i] < nums[i - 1]) {
-                    return;
-                }
-            }
             for (int i = 0; i < m; i++) {
-                sb.append(nums[i] + " ");
+                sb.append(arr[i] + " ");
             }
             sb.append("\n");
             return;
         }
-        for (int i = 0; i < n; i++) {
-            nums[depth] = p[i];
-            perm(depth + 1);
-            nums[depth] = 0;
+        for (int i = num; i <= n; i++) {
+            arr[depth] = i;
+            f(depth + 1, i);
         }
     }
 }
