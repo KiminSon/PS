bc9a6c0256744480a621fa15206f1403994da35a
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-26 01:04:00 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                sb.append("* ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+}
