e6d65a0d69d15d3b8c4c0be3e943ed755af01733
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-26 01:05:47 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            if (i != n - 1) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    sb.append(" ");
+                }
+            }
+            sb.append("*");
+            if (i != 0) {
+                for (int j = 0; j < 2 * (i - 1) + 1; j++) {
+                    sb.append(" ");
+                }
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 0; i < 2 * n - 1; i++) {
+            sb.append("*");
+        }
+        System.out.println(sb.toString());
+    }
+
+}
