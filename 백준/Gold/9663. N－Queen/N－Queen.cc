e9de55c7625144af42c5af44e36ea10c370775c1
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-26 19:38:12 +0900

    [Gold IV] Title: N-Queen, Time: 3800 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,35 +2,38 @@
 
 using namespace std;
 using ll = long long;
+int n, cnt = 0;
+int b[16] = {0,};
 
-int c[15];
-int n, t = 0;
-
-bool a(int l) {
-    for (int i = 0; i < l; i++)
-        if (c[i] == c[l] || abs(c[l] - c[i]) == l - i)
+bool p(int x) {
+    for (int i = 0; i < x; i++) {
+        if (b[x] == b[i] || x - i == abs(b[x] - b[i])) {
             return 0;
+        }
+    }
     return 1;
 }
 
-void q(int x) {
-    if (x == n)
-        t++;
-    else
-        for (int i = 0; i < n; i++) {
-            c[x] = i;
-            if (a(x))
-                q(x + 1);
+void f(int x) {
+    if (x == n) {
+        cnt++;
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        b[x] = i;
+        if (p(x)) {
+            f(x + 1);
         }
+    }
 }
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(NULL);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
     cin >> n;
-    q(0);
-    cout << t;
+    f(0);
+    cout << cnt;
 }
