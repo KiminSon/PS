5aab24ad3b1c11e001ba9bbdfdde047ae8fe90c7
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-26 17:16:42 +0900

    Update 재귀의 귀재.cc

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -3,28 +3,33 @@
 using namespace std;
 using ll = long long;
 
+
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-    int t;
-    cin >> t;
-    while (t--) {
+    int n;
+    cin >> n;
+    while (n--) {
         string s;
         cin >> s;
-        int cnt = 0;
-        int l = 0;
-        int r = s.size() - 1;
-        bool f = 1;
-        for (; l <= r; l++, r--) {
-            cnt++;
-            if (s[l] != s[r]) {
-                f = 0;
-                break;
+        string ss = s;
+        reverse(s.begin(), s.end());
+        if (s == ss) {
+            cout << 1 << ' ' << s.size() / 2 + 1 << '\n';
+        } else {
+            cout << 0 << ' ';
+            int cnt = 1;
+            for (int i = 0; i < s.size(); i++) {
+                if (s[i] == ss[i]) {
+                    cnt++;
+                } else {
+                    break;
+                }
             }
+            cout << cnt << '\n';
         }
-        cout << (f ? 1 : 0) << ' ' << (f && s.size() % 2 == 0 ? cnt + 1 : cnt) << '\n';
     }
 }
