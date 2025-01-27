a71c9b4b4056f8a884c6d8a80f5ad89b8ba43c02
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-27 17:19:40 +0900

    Update 미로 탐색.cc

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,37 +3,53 @@
 using namespace std;
 using ll = long long;
 
-int check[100][100];
 int n, m;
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
-string s[100];
+int arr[101][101] = {0,};
+int visited[101][101] = {0,};
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct loc {
+    int x = 0;
+    int y = 0;
+};
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
-        cin >> s[i];
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    check[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            char c;
+            cin >> c;
+            arr[i][j] = c - '0';
+        }
+    }
+    queue<loc> q;
+    loc l;
+    l.x = 1;
+    l.y = 1;
+    visited[l.x][l.y] = 1;
+    q.push(l);
     while (!q.empty()) {
-        auto c = q.front();
+        loc cur = q.front();
         q.pop();
         for (int i = 0; i < 4; i++) {
-            int nx = c.first + dx[i];
-            int ny = c.second + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+            if (nx < 1 || nx > n || ny < 1 || ny > m) {
                 continue;
-            if (s[nx][ny] == '0' || check[nx][ny] >= 1)
+            }
+            if (visited[nx][ny] || arr[nx][ny] == 0) {
                 continue;
-            check[nx][ny] = check[c.first][c.second] + 1;
+            }
+            visited[nx][ny] = visited[cur.x][cur.y] + 1;
             q.push({nx, ny});
         }
     }
-    cout << check[n - 1][m - 1];
+    cout << visited[n][m];
 }
