d849f10666d3d8308c7b952721d59087310fee83
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-27 22:50:06 +0900

    [Silver I] Title: 안전 영역, Time: 16 ms, Memory: 2448 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -3,25 +3,29 @@
 using namespace std;
 using ll = long long;
 
-int n, ans = 1, water = 0;
-int board[100][100];
-bool visited[100][100];
+int minW = 101, maxW = -1, ans = 1, n;
+int arr[100][100];
+int visited[100][100];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
+void re() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            visited[i][j] = 0;
+        }
+    }
+}
 
-void dfs(int x, int y) {
-    if (visited[x][y])
-        return;
+void dfs(int x, int y, int d) {
     visited[x][y] = 1;
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] <= d || visited[nx][ny] == 1) {
             continue;
-        if (board[nx][ny] < water)
-            continue;
-        dfs(nx, ny);
+        }
+        dfs(nx, ny, d);
     }
 }
 
@@ -32,28 +36,25 @@ int main() {
     freopen("../output.txt", "w", stdout);
 #endif
     cin >> n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> board[i][j];
-
-    while (1) {
-        water++;
-        int area = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                visited[i][j] = 0;
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (!visited[i][j] && board[i][j] >= water) {
-                    area++;
-                    dfs(i, j);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+            minW = min(minW, arr[i][j]);
+            maxW = max(maxW, arr[i][j]);
+        }
+    }
+    for (int i = minW - 1; i <= maxW + 1; i++) {
+        re();
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (arr[j][k] > i && visited[j][k] == 0) {
+                    dfs(j, k, i);
+                    cnt++;
                 }
-
-        if (area == 0)
-            break;
-        else
-            ans = max(ans, area);
+            }
+        }
+        ans = max(ans, cnt);
     }
     cout << ans;
 }
