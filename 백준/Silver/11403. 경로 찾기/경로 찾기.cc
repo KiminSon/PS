79c09361bf7cb3513b37b4b781a71c497c143e93
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-01-27 23:39:47 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (arr[j][i] && arr[i][k]) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
