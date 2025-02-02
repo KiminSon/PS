2d7070f0e5b8abb4592f4de57a012b66f0c44fd2
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-02-03 00:31:05 +0900

    Update 배열 돌리기.cc

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.cc
@@ -35,169 +35,45 @@ int main() {
         d /= 45;
 
         deque<vector<int> > dq;
-        vector<int> v;
-        int x = 0, y = 0;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
+        vector<vector<int> > v(8, vector<int>(n / 2));
+        int x = 0, y = n - 1, h = n / 2;
+        for (int i = 0; i < h; i++) {
+            v[0][i] = arr[x][x];
+            v[1][i] = arr[x][h];
+            v[2][i] = arr[x][y];
+            v[3][i] = arr[h][y];
+            v[4][i] = arr[y][y];
+            v[5][i] = arr[y][h];
+            v[6][i] = arr[y][x];
+            v[7][i] = arr[h][x];
             x += 1;
-            y += 1;
+            y += -1;
         }
-        v.clear();
 
-        x = 0, y = n / 2;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            x += 1;
+        for (int i = 0; i < 8 - d; i++) {
+            dq.push_back(v[i]);
         }
-        v.clear();
+        for (int i = 7; i >= 8 - d; i--) {
+            dq.push_front(v[i]);
+        }
+
+        for (int i = 0; i < 8; i++) {
+            v[i] = dq.front();
+            dq.pop_front();
+        }
 
         x = 0, y = n - 1;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
+        for (int i = 0; i < n / 2; i++) {
+            arr[x][x] = v[0][i];
+            arr[x][h] = v[1][i];
+            arr[x][y] = v[2][i];
+            arr[h][y] = v[3][i];
+            arr[y][y] = v[4][i];
+            arr[y][h] = v[5][i];
+            arr[y][x] = v[6][i];
+            arr[h][x] = v[7][i];
             x += 1;
             y += -1;
-        }
-        v.clear();
-
-        x = n / 2, y = n - 1;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            y += -1;
-        }
-        v.clear();
-
-        x = n - 1, y = n - 1;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            x += -1;
-            y += -1;
-        }
-        v.clear();
-
-        x = n - 1, y = n / 2;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            x += -1;
-        }
-        v.clear();
-
-        x = n - 1, y = 0;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            x += -1;
-            y += 1;
-        }
-        v.clear();
-
-        x = n / 2, y = 0;
-        while (1) {
-            if (x == n / 2 && y == n / 2) {
-                dq.push_back(v);
-                break;
-            }
-            v.push_back(arr[x][y]);
-            y += 1;
-        }
-
-        for (int i = 0; i < d; i++) {
-            dq.push_front(dq.back());
-            dq.pop_back();
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = 0, y = 0;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += 1;
-            y += 1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = 0, y = n / 2;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += 1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = 0, y = n - 1;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += 1;
-            y += -1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = n / 2, y = n - 1;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            y += -1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = n - 1, y = n - 1;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += -1;
-            y += -1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = n - 1, y = n / 2;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += -1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = n - 1, y = 0;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            x += -1;
-            y += 1;
-        }
-
-        v = dq.front();
-        dq.pop_front();
-        x = n / 2, y = 0;
-        for (int i = 0; i < n / 2; i++) {
-            arr[x][y] = v[i];
-            y += 1;
         }
 
         for (int i = 0; i < n; i++) {
