a6a1b05bf8f5793e6e918cdf8744cc6cff373d75
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-02-03 19:57:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = { 0, 0, 1, -1 };
+    static int[] dy = { 1, -1, 0, 0 };
+
+    public static class loc {
+        int x;
+        int y;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> lst = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && arr[i][j] == 1) {
+                    int cnt = 1;
+                    loc l = new loc();
+                    l.x = i;
+                    l.y = j;
+                    visited[i][j] = true;
+                    Queue<loc> q = new LinkedList();
+                    q.add(l);
+                    while (!q.isEmpty()) {
+                        loc cur = q.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = cur.x + dx[k];
+                            int ny = cur.y + dy[k];
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                                continue;
+                            }
+                            if (visited[nx][ny] || arr[nx][ny] == 0) {
+                                continue;
+                            }
+                            visited[nx][ny] = true;
+                            cnt++;
+                            loc tmp = new loc();
+                            tmp.x = nx;
+                            tmp.y = ny;
+                            q.add(tmp);
+                        }
+                    }
+                    lst.add(cnt);
+                }
+            }
+        }
+        Collections.sort(lst);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(lst.size() + "\n");
+        for (int i = 0; i < lst.size(); i++) {
+            sb.append(lst.get(i) + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
