f047db35ca99f97b7d48458dea615f6309d09a21
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-02-17 19:59:25 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2276 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -5,61 +5,55 @@ using ll = long long;
 
 struct pt {
     int x, y, cst;
-    bool operator>(const pt &other) const {
-        return cst > other.cst;
-    }
 };
+
+bool operator <(pt a, pt b) {
+    return a.cst > b.cst;
+}
 
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
+int arr[125][125];
+int visited[125][125];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-
     int t = 1;
-    while (true) {
+    while (1) {
         int n;
         cin >> n;
         if (n == 0) {
             break;
         }
-
-        vector<vector<int>> arr(n, vector<int>(n));
-        vector<vector<int>> visited(n, vector<int>(n, INT_MAX));
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 cin >> arr[i][j];
+                visited[i][j] = 2e9;
             }
         }
-
-        priority_queue<pt, vector<pt>, greater<>> q;
-        visited[0][0] = arr[0][0];
+        priority_queue<pt> q;
         q.push({0, 0, arr[0][0]});
-
-        while (!q.empty()) {
-            auto [x, y, cst] = q.top();
+        visited[0][0] = arr[0][0];
+        while (q.size()) {
+            auto cur = q.top();
             q.pop();
-            
-            if (visited[x][y] < cst) continue; // 불필요한 중복 방문 방지
-
+            int x = cur.x;
+            int y = cur.y;;
+            int cst = cur.cst;
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-
-                int new_cost = cst + arr[nx][ny];
-                if (visited[nx][ny] > new_cost) {
-                    visited[nx][ny] = new_cost;
-                    q.push({nx, ny, new_cost});
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                    continue;
+                }
+                int ncst = cst + arr[nx][ny];
+                if (visited[nx][ny] > ncst) {
+                    visited[nx][ny] = ncst;
+                    q.push({nx, ny, ncst});
                 }
             }
         }
