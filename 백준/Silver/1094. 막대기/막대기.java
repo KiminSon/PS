0f9036122dbfbf918d844f003dc9dcbac48c86a3
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-03-03 10:48:52 +0900

    [Silver V] Title: 막대기, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,21 +1,11 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = 64, cnt = 0, x = Integer.parseInt(br.readLine());
-        while (x > 0) {
-            if (x >= n) {
-                x -= n;
-                cnt++;
-            }
-            n >>= 1;
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append(cnt);
-        System.out.print(sb);
+        int x = Integer.parseInt(br.readLine());
+        System.out.print(Integer.bitCount(x));
     }
 }
