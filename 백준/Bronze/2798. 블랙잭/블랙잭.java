f4643776ef721d6542cbe17a946541b6a5572f41
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-03-04 20:46:25 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            arr[i] = tmp;
-        }
-        int ans = 0;
-        for (int i = 0; i < n - 2; i++) {
-            if (arr[i] > m) {
-                continue;
-            }
-            for (int j = i + 1; j < n - 1; j++) {
-                if (arr[i] + arr[j] > m) {
-                    continue;
-                }
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == m) {
-                        System.out.println(m);
-                        return;
-                    }
-                    if (arr[i] + arr[j] + arr[k] < m) {
-                        ans = Math.max(ans, arr[i] + arr[j] + arr[k]);
-                    }
-                }
-            }
-        }
-        System.out.println(ans);
-    }
+	static int n, m, ans;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		// --------------솔루션 코드를 작성하세요.--------------------------------
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		f(0, 0, 0);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(ans);
+		System.out.print(sb);
+	}
+
+	static void f(int d, int nxt, int c) {
+		if (c > m) {
+			return;
+		}
+		if (d == 3) {
+			ans = Math.max(ans, c);
+			return;
+		}
+		for (int i = nxt; i < n; i++) {
+			f(d + 1, i + 1, c + arr[i]);
+		}
+	}
 
 }
