c50c60ad510d1348c46b4559c935fc963037e4df
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-03-05 23:21:29 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -5,40 +5,39 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[26];
-        String s = br.readLine();
-        int sz = s.length();
-        
-        for (int i = 0; i < sz; i++) {
-            arr[s.charAt(i) - 'A']++;
+        char[] input = br.readLine().toCharArray();
+        int[] count = new int[26];
+        int length = input.length;
+
+        for (char ch : input) {
+            count[ch - 'A']++;
         }
 
         int odd = 0;
         char oddChar = 0;
         for (int i = 0; i < 26; i++) {
-            if ((arr[i] & 1) == 1) {
+            if ((count[i] & 1) == 1) {
                 odd++;
                 oddChar = (char) (i + 'A');
                 if (odd > 1) {
                     System.out.print("I'm Sorry Hansoo");
                     return;
                 }
-                arr[i]--;
-            }
-        }
-        
-        char[] result = new char[sz];
-        int left = 0, right = sz - 1;
-        
-        for (int i = 0; i < 26; i++) {
-            while (arr[i] > 0) {
-                result[left++] = (char) (i + 'A');
-                result[right--] = (char) (i + 'A');
-                arr[i] -= 2;
             }
         }
 
-        if (oddChar != 0) {
+        char[] result = new char[length];
+        int left = 0, right = length - 1;
+        
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 1) {
+                result[left++] = (char) (i + 'A');
+                result[right--] = (char) (i + 'A');
+                count[i] -= 2;
+            }
+        }
+
+        if (odd == 1) {
             result[left] = oddChar;
         }
 
