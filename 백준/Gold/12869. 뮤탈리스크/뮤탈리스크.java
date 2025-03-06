7ec528962f89af1f581575597f35c0cf9a6a1a06
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-03-06 23:56:38 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 68 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -3,50 +3,42 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] d = {
-            {9, 3, 1},
-            {9, 1, 3},
-            {1, 3, 9},
-            {3, 1, 9},
-            {3, 9, 1},
-            {1, 9, 3}
-    };
-
-    static int n;
-    static int[] arr = new int[3];
-    static boolean[][][] visited;
-
     public static void main(String[] args) throws Exception {
+        byte[][] d = {
+                {9, 3, 1},
+                {9, 1, 3},
+                {1, 3, 9},
+                {3, 1, 9},
+                {3, 9, 1},
+                {1, 9, 3}
+        };
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        byte n = Byte.parseByte(br.readLine());
+        byte[] arr = new byte[3];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (byte i = 0; i < n; i++) {
+            arr[i] = Byte.parseByte(st.nextToken());
         }
-        visited = new boolean[arr[0] + 1][arr[1] + 1][arr[2] + 1];
-        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][][] visited = new boolean[arr[0] + 1][arr[1] + 1][arr[2] + 1];
+        Queue<byte[]> q = new ArrayDeque<>();
         visited[arr[0]][arr[1]][arr[2]] = true;
-        q.add(new int[]{arr[0], arr[1], arr[2], 1});
+        q.add(new byte[]{arr[0], arr[1], arr[2], 1});
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int z = cur[2];
-            int cnt = cur[3];
-            for (int i = 0; i < 6; i++) {
-                int nx = x - d[i][0] < 0 ? 0 : x - d[i][0];
-                int ny = y - d[i][1] < 0 ? 0 : y - d[i][1];
-                int nz = z - d[i][2] < 0 ? 0 : z - d[i][2];
+            byte[] cur = q.poll();
+            for (byte i = 0; i < 6; i++) {
+                byte nx = cur[0] - d[i][0] < 0 ? 0 : (byte) (cur[0] - d[i][0]);
+                byte ny = cur[1] - d[i][1] < 0 ? 0 : (byte) (cur[1] - d[i][1]);
+                byte nz = cur[2] - d[i][2] < 0 ? 0 : (byte) (cur[2] - d[i][2]);
 
                 if (visited[nx][ny][nz]) {
                     continue;
                 }
                 visited[nx][ny][nz] = true;
                 if (nx == 0 && ny == 0 && nz == 0) {
-                    System.out.println(cnt);
+                    System.out.println(cur[3]);
                     return;
                 }
-                q.add(new int[]{nx, ny, nz, cnt + 1});
+                q.add(new byte[]{nx, ny, nz, (byte) (cur[3] + 1)});
             }
         }
     }
