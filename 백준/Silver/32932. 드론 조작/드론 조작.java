b6feb37c8d7b479de2091394ba51a83fe6972f67
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-03-23 21:03:14 +0900

    [Silver V] Title: 드론 조작, Time: 76 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Silver/32932. 드론 조작/드론 조작.java
+++ 백준/Silver/32932. 드론 조작/드론 조작.java
@@ -2,48 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
         int x = 500, y = 500;
         Set<Integer> w = new HashSet<>();
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()) + 500;
             int b = Integer.parseInt(st.nextToken()) + 500;
             w.add(a * 1001 + b);
         }
+
         String s = br.readLine();
         for (int i = 0; i < k; i++) {
-            int nx = x;
-            int ny = y;
+            char move = s.charAt(i);
+            int nx = x, ny = y;
 
-            if (s.charAt(i) == 'R') {
-                nx++;
-                if (w.contains(nx * 1001 + ny)) {
-                    continue;
-                }
+            switch (move) {
+                case 'R':
+                    nx++;
+                    break;
+                case 'L':
+                    nx--;
+                    break;
+                case 'U':
+                    ny++;
+                    break;
+                case 'D':
+                    ny--;
+                    break;
+            }
+
+            if (!w.contains(nx * 1001 + ny)) {
                 x = nx;
-            } else if (s.charAt(i) == 'L') {
-                nx--;
-                if (w.contains(nx * 1001 + ny)) {
-                    continue;
-                }
-                x = nx;
-            } else if (s.charAt(i) == 'U') {
-                ny++;
-                if (w.contains(nx * 1001 + ny)) {
-                    continue;
-                }
-                y = ny;
-            } else {
-                ny--;
-                if (w.contains(nx * 1001 + ny)) {
-                    continue;
-                }
                 y = ny;
             }
         }
