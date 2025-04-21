4bc7af67b29293dccecaeb40c827abcd113e0f21
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-04-21 19:40:59 +0900

    [Gold V] Title: 행복 유치원, Time: 468 ms, Memory: 64392 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] diff = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+
+        Arrays.sort(diff);
+
+        int sum = 0;
+        for (int i = 0; i < n - k; i++) {
+            sum += diff[i];
+        }
+
+        System.out.print(sum);
+    }
+}
