068bb6ddae397ffa60ad4bc28e425d64006ea22c
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-07-26 22:52:27 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 556 ms, Memory: 103220 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,78 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Pt implements Comparable<Pt> {
-        int x, y, d, cst;
-
-        public Pt(int x, int y, int d, int cst) {
-            this.x = x;
-            this.y = y;
-            this.d = d;
-            this.cst = cst;
-        }
-
-        @Override
-        public int compareTo(Pt o) {
-            return Integer.compare(this.cst, o.cst);
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n, m, k;
-        int[][][] visited;
-        int[][] arr;
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
         int[] dx = {0, 0, 1, -1};
         int[] dy = {1, -1, 0, 0};
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = 1;
-        visited = new int[k + 1][n][m];
-        arr = new int[n][m];
+
+        boolean[][] arr = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
+            String tmp = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = s.charAt(j) - '0';
-                for (int l = 0; l <= k; l++) {
-                    visited[l][i][j] = Integer.MAX_VALUE - 1000;
+                arr[i][j] = tmp.charAt(j) == '1';
+            }
+        }
+
+        boolean[][][] visited = new boolean[n][m][2];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0, 1, 0});
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], d = cur[2], w = cur[3];
+
+            if (x == n - 1 && y == m - 1) {
+                System.out.println(d);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    continue;
+                }
+
+                if (!arr[nx][ny] && !visited[nx][ny][w]) {
+                    q.add(new int[]{nx, ny, d + 1, w});
+                    visited[nx][ny][w] = true;
+                }
+
+                if (arr[nx][ny] && w == 0 && !visited[nx][ny][1]) {
+                    q.add(new int[]{nx, ny, d + 1, 1});
+                    visited[nx][ny][1] = true;
                 }
             }
         }
 
-        PriorityQueue<Pt> q = new PriorityQueue<>();
-        q.add(new Pt(0, 0, 0, 0));
-        visited[0][0][0] = 0;
-        while (!q.isEmpty()) {
-            Pt cur = q.poll();
-            if (cur.x == n - 1 && cur.y == m - 1) {
-                System.out.print(cur.cst + 1);
-                return;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                if (arr[nx][ny] == 1) {
-                    if (cur.d >= k) {
-                        continue;
-                    }
-                    if (visited[cur.d + 1][nx][ny] > visited[cur.d][cur.x][cur.y] + 1) {
-                        visited[cur.d + 1][nx][ny] = visited[cur.d][cur.x][cur.y] + 1;
-                        q.add(new Pt(nx, ny, cur.d + 1, visited[cur.d + 1][nx][ny]));
-                    }
-                } else if (visited[cur.d][nx][ny] > visited[cur.d][cur.x][cur.y] + 1) {
-                    visited[cur.d][nx][ny] = visited[cur.d][cur.x][cur.y] + 1;
-                    q.add(new Pt(nx, ny, cur.d, visited[cur.d][nx][ny]));
-                }
-            }
-        }
-        System.out.print(-1);
+        System.out.println(-1);
     }
 }
