74b9fb3e768d96ac33e68bb6856b51d89cb81d75
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-07-30 19:35:16 +0900

    [Silver III] Title: 타노스, Time: 104 ms, Memory: 14144 KB, Score: 25 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -1,44 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] c = br.readLine().toCharArray();
-        int n = c.length;
-        boolean[] b = new boolean[c.length];
-        int zcnt = 0, ocnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (c[i] == '0') {
-                zcnt++;
-            } else {
-                ocnt++;
-            }
-        }
-        zcnt /= 2;
-        ocnt /= 2;
-        int idx = 0;
-        for (int i = 0; i < ocnt; i++) {
-            if (c[idx++] == '1') {
-                b[idx - 1] = true;
-            } else {
-                i--;
-            }
-        }
-        idx = n - 1;
-        for (int i = 0; i < zcnt; i++) {
-            if (c[idx--] == '0') {
-                b[idx + 1] = true;
-            } else {
-                i--;
-            }
-        }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (!b[i]) {
-                sb.append(c[i]);
+        String s = br.readLine();
+        int oc = 0;
+        int zc = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                zc++;
+            } else {
+                oc++;
             }
         }
+        sb.append("0".repeat(Math.max(0, zc / 2))).append("1".repeat(Math.max(0, oc / 2)));
         System.out.print(sb);
     }
 }
