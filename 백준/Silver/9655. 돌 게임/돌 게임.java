b6865050c808e08dcdcafc08f1f51bc97496d187
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-08-17 15:17:17 +0900

    [Silver V] Title: 돌 게임, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print((Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine()) & 1) == 1 ? "SK" : "CY");
+    }
+}
